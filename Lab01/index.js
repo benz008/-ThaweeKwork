15d637762dab770c20cbbfeed2f6183386c2dfda
benz008 <thawee.k@psru.ac.th>
2019-12-08 00:19:15 +0700

    initial commit

---
+++ Lab01/index.js
@@ -0,0 +1,21 @@
+console.log("thawee konkaew");
+console.log("ทวี ก้อนแก้ว");   
+//alert("Please refresh");
+let firstName = "benz";
+let lastName = "Konkaew";
+let age=15;
+let height = 59.5;
+
+//function (number)
+//{
+//    return number*number
+//}
+//let result = squareX(7);
+//squareX(7);
+
+function circlearea(number)
+{
+    return 3.14 *(number*2)
+}
+
+let result = circlearea(number);
